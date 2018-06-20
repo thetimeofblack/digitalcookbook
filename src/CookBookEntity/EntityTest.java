c554503b19a343257328880a03b6eb1de03d2822
18393 <18393@172.31.50.61>
2018-06-20 22:35:07 +0800

    yjx

--- src/CookBookEntity/EntityTest.java
+++ src/CookBookEntity/EntityTest.java
@@ -3,7 +3,7 @@ import DigitalCookbook.CookBookApp;
 public class EntityTest {
 	public static void main(String[]  args) {
 		Recipe recipe = CookBookApp.createGongBaoJiding();
-		
+		System.out.println("heo");
 		recipe.show();
 	}
 }
