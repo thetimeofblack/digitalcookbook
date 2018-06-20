f480fa334054721472fc1e40a50ce70734bc4264
thetimeofblack <1191401005@qq.com>
2018-06-20 23:23:09 +0800

    detailController

--- src/CookBookView/firstview/fvController.java
+++ src/CookBookView/firstview/fvController.java
@@ -34,7 +34,7 @@ public class fvController {
 	}
 
 	public void loginView(ActionEvent event) throws IOException {
-		FXMLLoader loader = new FXMLLoader(getClass().getResource("login.fxml"));
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("../loginview/login.fxml"));
 		GridPane root = (GridPane) loader.load();
 		DatabaselayerObject dao = new DatabaselayerObject();	
 		
