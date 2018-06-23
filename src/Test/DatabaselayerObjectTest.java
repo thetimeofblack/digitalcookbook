48e7958f0d4a84832714d6a482b071eab9c1d311
Administrator <Administrator@DESKTOP-PC7OP7N>
2018-06-23 09:11:50 +0200

    addTestpackage

---
+++ src/Test/DatabaselayerObjectTest.java
@@ -0,0 +1,143 @@
+package Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import CookBookDataBaseAcess.DatabaselayerObject;
+class DatabaselayerObjectTest {
+	private DatabaselayerObject dao ; 
+	@BeforeAll 
+	void  initialize(){
+		this.dao = new DatabaselayerObject();
+		
+	}
+	
+	
+	@Test
+	void testDatabaselayerObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testUserLogin() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testUserLogin1() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testUserRegisterUser() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testUserRegister() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetUserRecipe() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetallrecipelist() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testShowsearchingrecipelist() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testInsertrecipe() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testInsertingredients() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testInsertpreparationsteps() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testInsertrecipeuser() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testInsertRecipe() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDeleterecipe() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDeleteingredients() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDeletepreparationsteps() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testJudgerecipeuser() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDeleterecipeuser() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDeleteRecipe() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testEditRecipe() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testInsertfavourite() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testJudgefavourite() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSaveRecipeDescription() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSaveCommentandRate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testClose() {
+		fail("Not yet implemented");
+	}
+
+}
